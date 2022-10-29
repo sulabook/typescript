1df72a42b65ef4207a6a873f17eec9a5b7aad380
sulabook <sula.book@gmail.com>
2022-10-29 16:47:40 +0900

    random

--- ch02-2/src/person/Person.ts
+++ ch02-2/src/person/Person.ts
@@ -1,4 +1,4 @@
-let MAX_AGE = 100
+import * as U from "../utils/makeRandomNumber"
 
 export interface IPerson {
     name: string
@@ -9,9 +9,5 @@ class Person implements IPerson {
     constructor(public name: string, public age: number) {}
 }
 
-function makeRandomNumber(max: number = MAX_AGE): number {
-    return Math.ceil((Math.random() * max))
-}
-
 export const makePerson = (name: string,
-    age: number = makeRandomNumber()) => ({name, age})
+    age: number = U.makeRandomNumber()) => ({name, age})
