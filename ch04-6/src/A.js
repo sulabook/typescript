4e4f374f44a4ee96657f7ef959ef3f8c8c41d47e
sulabook <sula.book@gmail.com>
2022-11-02 23:13:56 +0900

    A

---
+++ ch04-6/src/A.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.A = void 0;
+var A = /** @class */ (function () {
+    function A() {
+        this.value = 1;
+        this.method = function () {
+            console.log("value: ".concat(this.value));
+        };
+    }
+    return A;
+}());
+exports.A = A;
---
+++ ch04-6/src/testA.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var A_1 = require("./A");
+var a = new A_1.A;
+a.method();
