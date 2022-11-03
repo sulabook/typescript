ff83793cfd99e31b117560c12351f87d34dbbd74
sulabook <sula.book@gmail.com>
2022-11-03 23:22:52 +0900

    split-test

---
+++ ch05-1/src/split-test.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var split_1 = require("./split");
+console.log((0, split_1.split)('hello'), (0, split_1.split)('h_e_l_l_o', '_'));
---
+++ ch05-1/src/split.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.split = void 0;
+var split = function (str, delim) {
+    if (delim === void 0) { delim = ''; }
+    return str.split(delim);
+};
+exports.split = split;
