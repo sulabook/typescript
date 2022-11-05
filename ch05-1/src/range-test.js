c18c33bc07dbe42fc51a556e9cc4803af85ef16c
sulabook <sula.book@gmail.com>
2022-11-05 15:04:49 +0900

    range

---
+++ ch05-1/src/range-test.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var range_1 = require("./range");
+var numbers = (0, range_1.range)(1, 9 + 1);
+console.log(numbers);
---
+++ ch05-1/src/range.js
@@ -0,0 +1,16 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+exports.__esModule = true;
+exports.range = void 0;
+var range = function (from, to) {
+    return from < to ? __spreadArray([from], (0, exports.range)(from + 1, to), true) : [];
+};
+exports.range = range;
